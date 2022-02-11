cf3996ecc0b95b473908184a09feab8fdff32579
Arpit Sureka <surekaarpit@gmail.com>
2022-02-11 23:58:25 +0530

    updates

--- auth.js
+++ auth.js
@@ -1,12 +1,12 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
+require('dotenv').config();
 
-const GOOGLE_CLIENT_ID = '653237617296-j4n76flsbmkoiq5q2ojufbgm06rkdf4d.apps.googleusercontent.com';
-const GOOGLE_CLIENT_SECRET = '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';
+
 
 passport.use(new GoogleStrategy({
-  clientID: GOOGLE_CLIENT_ID,
-  clientSecret: GOOGLE_CLIENT_SECRET,
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   callbackURL: "http://localhost:3000/auth/google/callback",
   passReqToCallback: true,
 },
